1b7b782eb96c1026fe94adc3118c079189dfaee1
jinho6225 <jinho6225@hotmail.com>
2020-02-18 20:11:07 -0800

    initial commit

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,21 @@
-//TODO
-/*
-<div>
-  Search goes here
-  List goes here
-</div>
-*/
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+
+  render() {
+    const { pokemon } = this.props
+    console.log(pokemon)
+    return (
+        <div>
+          Search goes here
+          List goes here
+        </div>
+    )
+  }
+}
+
+export default App;
